2e9e91e0996e7d01e36714e74ced2fd0dd2ff3e6
achille1017 <achillemarseille@gmail.com>
2023-12-14 19:16:36 +0100

    saving configuration

--- db.py
+++ db.py
@@ -78,7 +78,6 @@ def update_config(config: str, organisation: str) -> bool:
     conn = sqlite3.connect('db.sqlite')
     logging.info("trying to update %s config",organisation)
     c = conn.cursor()
-    print("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAa")
     c.execute("update organisations set configured=1 , config=('{config}') where name='{organisation}'".format(
         config=config, organisation=organisation))
     conn.commit()
@@ -169,6 +168,9 @@ def read_config(email: str) -> dict:
     rows = c.fetchall()
     if len(rows) < 1 or rows[0][0] == None:
         return None
+    logging.info(type(rows[0][0]))
+    logging.info(rows[0][0])
+    
     return json.loads(rows[0][0])
 
 
