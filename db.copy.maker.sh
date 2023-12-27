46150e018291b8d08b453c561fdc01392cb5e2ae
achille1017 <achillemarseille@gmail.com>
2023-12-27 21:22:38 +0100

    db copies

---
+++ db.copy.maker.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Set the source and destination directories
+source_dir="/home/achille/wallet-provider"
+destination_dir="/home/achille/wallet-provider/backup"
+
+# Set the file name
+file_name="db.sqlite"
+
+# Create the destination directory if it doesn't exist
+mkdir -p "$destination_dir"
+
+# Copy the file to the destination directory and append the current date to the file name
+cp "$source_dir/$file_name" "$destination_dir/$file_name_$(date +'%Y-%m-%d')"
